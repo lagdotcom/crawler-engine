d87324d7b60faba32c42efe31056f869ba86d46f
Paul Davies <lagdotcom@gmail.com>
2021-08-24 14:52:53 +0100

    added lighting! it makes things behind translucent walls bug out

--- src/GeometryCache.ts
+++ src/GeometryCache.ts
@@ -1,49 +1,27 @@
-import { BufferAttribute, BufferGeometry } from "three";
+import { BufferGeometry, PlaneBufferGeometry } from "three";
 
 import AbstractCache from "./AbstractCache";
 
+const hpi = Math.PI / 2;
+
 export default class GeometryCache extends AbstractCache<BufferGeometry> {
   flat(h: number): BufferGeometry {
-    return this.get(`f${h}`, () => {
-      const s = 0.5;
-      const vertices = [-s, h, -s, s, h, -s, s, h, s, -s, h, s];
-      const faces = [0, 1, 3, 1, 2, 3];
-
-      return this.asGeometry(faces, vertices);
-    });
+    return this.get(`f${h}`, () =>
+      new PlaneBufferGeometry(1, 1).rotateX(hpi).translate(0, h, 0)
+    );
   }
 
   horizontal(y: number, h: number, l: number): BufferGeometry {
-    return this.get(`h${y}:${h}:${l}`, () => {
-      const s = 0.5;
-
-      const vertices = [-s, h, y, s, h, y, s, l, y, -s, l, y];
-      const faces = [0, 1, 3, 1, 2, 3];
-
-      return this.asGeometry(faces, vertices);
-    });
+    return this.get(`h${y}:${h}:${l}`, () =>
+      new PlaneBufferGeometry(1, h - l).translate(0, (h + l) / 2, y)
+    );
   }
 
   vertical(x: number, h: number, l: number): BufferGeometry {
-    return this.get(`v${x}:${h}:${l}`, () => {
-      const s = 0.5;
-
-      const vertices = [x, h, s, x, h, -s, x, l, -s, x, l, s];
-      const faces = [0, 1, 3, 1, 2, 3];
-
-      return this.asGeometry(faces, vertices);
-    });
-  }
-
-  private asGeometry(faces: number[], vertices: number[]) {
-    const geometry = new BufferGeometry();
-
-    geometry.setIndex(faces);
-    geometry.setAttribute(
-      "position",
-      new BufferAttribute(new Float32Array(vertices), 3)
+    return this.get(`v${x}:${h}:${l}`, () =>
+      new PlaneBufferGeometry(1, h - l)
+        .rotateY(hpi)
+        .translate(x, (h + l) / 2, 0)
     );
-
-    return geometry;
   }
 }
