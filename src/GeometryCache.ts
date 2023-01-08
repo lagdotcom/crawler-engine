72abe9ff46d5e28437a805f41fa84ce742c80cde
Paul Davies <lagdotcom@gmail.com>
2023-01-08 15:56:32 +0000

    deps

--- src/GeometryCache.ts
+++ src/GeometryCache.ts
@@ -1,4 +1,4 @@
-import { BufferGeometry, PlaneBufferGeometry } from "three";
+import { BufferGeometry, PlaneGeometry } from "three";
 
 import AbstractCache from "./AbstractCache";
 
@@ -7,23 +7,19 @@ const hpi = Math.PI / 2;
 export default class GeometryCache extends AbstractCache<BufferGeometry> {
   flat(h: number): BufferGeometry {
     return this.get(`f${h}`, () =>
-      new PlaneBufferGeometry(1, 1).rotateX(hpi).translate(0, h, 0)
+      new PlaneGeometry(1, 1).rotateX(hpi).translate(0, h, 0)
     );
   }
 
   horizontal(y: number, h: number, l: number): BufferGeometry {
     return this.get(`h${y}:${h}:${l}`, () =>
-      new PlaneBufferGeometry(1, h - l)
-        .rotateX(Math.PI)
-        .translate(0, (h + l) / 2, y)
+      new PlaneGeometry(1, h - l).rotateX(Math.PI).translate(0, (h + l) / 2, y)
     );
   }
 
   vertical(x: number, h: number, l: number): BufferGeometry {
     return this.get(`v${x}:${h}:${l}`, () =>
-      new PlaneBufferGeometry(1, h - l)
-        .rotateY(-hpi)
-        .translate(x, (h + l) / 2, 0)
+      new PlaneGeometry(1, h - l).rotateY(-hpi).translate(x, (h + l) / 2, 0)
     );
   }
 }
